23d6c8f8618dcd828beecc2587d088f100d230ab
Ofren Dialsa <153308025+OfrenDialsa@users.noreply.github.com>
2024-10-13 13:40:50 +0700

    Update ApiConfig.kt

--- app/src/main/java/com/example/mydicodingevent/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/mydicodingevent/data/retrofit/ApiConfig.kt
@@ -14,7 +14,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://event-api.dicoding.dev")
+                .baseUrl("")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
